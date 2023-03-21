a16118a52b62a94e86a62a5fdbff491a6c496e60
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-03-21 17:46:26 -0500

    Updated JS Code

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,11 @@
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(',')','*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +18,28 @@ function writePassword(maxLengthPass) {
 
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Test
+// Password details
+function generatePassword(){
+  var passwordLength = parseInt(
+    prompt('How long of a password would you like?')
+  );
+if (Number.isNaN(passwordLength)) {
+   alert('Password length must be a number');
+   return null;
+ }
+
+ if (passwordLength < 8){
+  alert('Password should be at least 8 characters long');
+  return null;
+ }
+
+ if (passwordLength > 128) {
+  alert('Password must be no more than 128 characters long');
+  return null;
+ }
+
+
+}
